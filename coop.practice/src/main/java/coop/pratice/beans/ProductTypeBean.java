ba4e019c18d8919ba251ef6f683ba18af305514f
kengpong36 <kengpong36@gmail.com>
2015-05-18 14:26:08 +0700

    product type 2(hashmap)

--- coop.practice/src/main/java/coop/pratice/beans/ProductTypeBean.java
+++ coop.practice/src/main/java/coop/pratice/beans/ProductTypeBean.java
@@ -1,8 +1,10 @@
 package coop.pratice.beans;
 
+import java.util.HashMap;
 import java.util.List;
 import java.io.Serializable;
 
+import org.hibernate.event.spi.ClearEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -15,27 +17,47 @@ import coop.pratice.domain.User;
 @Scope
 public class ProductTypeBean implements Serializable {
 
-
+	
 
 	@Autowired
 	private ProductTypeService productTypeService;
 
-	public List<Producttype> producttypes   ;
+	private HashMap<String, Integer> hashProductType;
 	
+	
+
+	public List<Producttype> producttypes;
+
 	public void setProducttypes(List<Producttype> producttypes) {
 		this.producttypes = producttypes;
 	}
 
 	private Producttype producttype = new Producttype();
 
-	public void addProductType() {
+	public String addProductType() {
 		getProductTypeService().addProductType(producttype);
-		setProducttype(null);
+		setProducttype(new Producttype());
+		
+		return "list1";
+
 	}
 
-	public void updateProductType() {
+	public String updateProductType() {
 		getProductTypeService().updateProductType(getProducttype());
 		setProducttype(null);
+		return "list";
+	}
+
+	public String goToEditPage(Producttype producttype) {
+		setProducttype(producttype);
+		return "editpage";
+
+	}
+
+	public String goToAddPage() {
+		setProducttype(producttype);
+		return "addpage";
+
 	}
 
 	public void deleteProductType(Producttype producttype) {
@@ -43,10 +65,9 @@ public class ProductTypeBean implements Serializable {
 	}
 
 	public List<Producttype> getProducttypes() {
-	
-		
+
 		List<Producttype> list = getProductTypeService().getProducttypes();
-		
+
 		return list;
 	}
 
@@ -66,4 +87,23 @@ public class ProductTypeBean implements Serializable {
 		this.producttype = producttype;
 	}
 
+	public void clear(Producttype producttype) {
+		producttype.setName("");
+	}
+
+	public HashMap<String, Integer> getHashProductType() {
+		
+		hashProductType = new HashMap<String, Integer>();
+		
+		List<Producttype> listProductType = getProductTypeService().getProducttypes();
+		
+		for (Producttype item : listProductType) {
+			
+			hashProductType.put(item.getName(),item.getId());
+		}
+		return hashProductType;
+		
+	}
+	
+	
 }
